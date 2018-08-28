0a29efc2ab9817373ad144c7fab924f8d232f08a
Manislawa <malwina.witoszek82@gmail.com>
2018-08-28 11:11:36 +0200

    Scaling for typography - responsive headers

--- src/js/scrolleffect.js
+++ src/js/scrolleffect.js
@@ -1,7 +1,9 @@
 function scrollEffect(){
     var header = document.getElementById('header'),
-        li = header.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.children,
+        li = header.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild.children,
+        // li = header.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.children,
         ypos = window.pageYOffset;
+        console.log(li);
 
     if(ypos > 587){
         header.classList.add('scroll-header');  //header configuration after scroll
@@ -13,7 +15,7 @@ function scrollEffect(){
         for(var i=0; i<li.length; i++){                 //header-nav-li base configuration
             li[i].classList.remove('scroll-header-li');
         }
-    }    
+    }
 }
 window.addEventListener('scroll',scrollEffect);
 
@@ -28,7 +30,7 @@ window.addEventListener('scroll',scrollEffect);
 //     }else{
 //         logo.classList.remove('scroll-header-logo'); //header base configuration
 //         // header.firstElementChild.firstElementChild.classList.remove('scroll-header-logo');  //header-logo configuration after scroll
-//     }    
+//     }
 // }
 
 // window.addEventListener('scroll',scrollLogoEffect);
